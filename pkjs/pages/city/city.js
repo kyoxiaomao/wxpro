8a8b821b412c90ff2853dc959c44e9764e7bc12e
汪军 <kyoxiaomao@163.com>
2018-08-05 17:41:54 +0800

    201808051741

--- pkjs/pages/city/city.js
+++ pkjs/pages/city/city.js
@@ -1,175 +1,129 @@
-//index.js
-//获取应用实例
-//调试：T:代表配置，F：代表点击事件，
-var app = getApp()
+// pages/city/city.js
+var innerAudioContext = wx.createInnerAudioContext();
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    items: [
-      { name: 'dy', value: '地域', checked: 'true' }, 
-      { name: 'hy', value: '行业' }, 
-    ],
-    tjhdIMGWW: "",//推荐活动图片宽度
-    tjhdIMGHH: "",//推荐活动图片高度
-    hdImgHH: "",//活动图片高度
-    wwSystemWidth: "",//设备宽度
-    whSystemHeight: "",//设备高度
-    dhtxtClass01: "dh-txt01-false",//导航01
-    dhtxtClass02: "dh-txt01-false",//导航02
-    dhtxtClass03: "dh-txt01-true",//导航03
-    dhtxtClass04: "dh-txt01-false",//导航04
-    dhbottom: false,//底部导航
-    dhtop: true,//顶部导航
- 
+    syWidth:"",//系统宽度
+    syHeight:"",//系统高度
+    kgUrlMusic:"on-laba-kg",//音乐开关按钮变换
+    lbUrlMusic: "on-laba",//喇叭开关按钮变换
   },
-  onLoad: function () {
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
     var that = this
     //获取设备宽高
     wx.getSystemInfo({
       success: function (res) {
-        var _tjhdIMGWW = res.windowWidth * 0.3;
         that.setData({
-          wwSystemWidth: res.windowWidth,
-          whSystemHeight: res.windowHeight,
-
-          tjhdIMGWW: _tjhdIMGWW,
-          tjhdIMGHH: 358 / (247 / _tjhdIMGWW),
-          hdImgHH: 300 / (650 / res.windowWidth),
+          syWidth: res.windowWidth,
+          syHeight: res.windowHeight,
         })
       }
     })
-    console.log("T00:设备宽度" + that.data.wwSystemWidth + "|设备高度" + that.data.whSystemHeight)
-    
+   // this.onMusic();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+  //播放音乐
+  onMusic:function(){
    
-    this.dhInit();//初始化导航
-    this.loadData(10);//加载内容
-
+    innerAudioContext.autoplay = true
+    innerAudioContext.loop =true
+    innerAudioContext.src = 'https://www.citytk.com/citytk/pkjs/audio/tuange.mp3'
+    innerAudioContext.onPlay(() => {
+      console.log('开始播放')
+    })
+    innerAudioContext.onError((res) => {
+      console.log(res.errMsg)
+      console.log(res.errCode)
+    })
+   
+  },
+//跳转到PK页面
+pkonClick:function(){
+  wx.navigateTo({
+    url: '../index/index'
+  })
+},
+  //跳转到lx页面
+  lxonClick: function () {
 
   },
-  radioChange: function (e) {
-    console.log('radio发生change事件，携带value值为：', e.detail.value)
-  }
-  ,
-  loadData: function (count) {
-   /* var that = this
-    wx.request({
-      url: 'https://www.citytk.com/djyhphp/mode.php',
-      header: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      data: {
-        mode: "getdjyhdata",
-      },
-      method: "POST",
-      success: function (res) {
-        console.log("获取独家优惠数据：" + res.data.length)
-        if (res.data.length<count){
-          count = res.data.length;
-        } 
-        if (res.data != "") {
-          for (let i = 0; i < count; i++) {
-            let obj = {};
-            obj.viewId = res.data[i].spid;
-            obj.imgutl01 = res.data[i].imgurl01;
-            obj.djyhtitle = res.data[i].sptitle.substring(0, 24);
-            if (res.data[i].dplx = "天猫") {
-              obj.logoimgutl01 = "../images/dptb.jpg";
-            } else {
-              obj.logoimgutl01 = "../images/dptb.jpg";
-            }
-            obj.price = res.data[i].price;
-            var stryy= res.data[i].purchase;
-            if (stryy.length>4){
-              obj.purchase = stryy.substr(0, stryy.length - 4) + "." + stryy.substr(stryy.length-4, 1)+"万";
-            }else{
-              obj.purchase = stryy;
-            }
-           
-            obj.quanPrice = res.data[i].quanPrice;
-            obj.day = res.data[i].outtime;
-           
-            that.data.djyhData.push(obj);
-          }
-          that.setData({
-            djyhData: that.data.djyhData,
-          })
-
-        } else {
-          console.log("获取独家优惠数据为空")
-        }
-      },
-      fail: function (e) {
-        console.log("获取独家优惠数据失败e:" + e.errMsg)
+//背景音乐开关按钮
+kgMusicOnClick:function(){
+ 
+    var that=this;
+    if (that.data.kgUrlMusic =="on-laba-kg"){
+      var _kgUrlMusic ="off-laba-kg";
+      var _lbUrlMusic = "off-laba";
+      that.setData({
+        kgUrlMusic: _kgUrlMusic,
+        lbUrlMusic: _lbUrlMusic,
+      })
+      console.log("关闭音乐")
+      innerAudioContext.stop();
+      }else{
+      var _kgUrlMusic = "on-laba-kg";
+      var _lbUrlMusic = "on-laba";
+      that.setData({
+        kgUrlMusic: _kgUrlMusic,
+        lbUrlMusic: _lbUrlMusic,
+      })
+      console.log("开启音乐")
+      innerAudioContext.play();
       }
-    })
-    console.log("加载内容完毕")*/
   },
-  //下拉事件
+ 
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
   onPullDownRefresh: function () {
-    console.log("下拉")
+  
   },
-  //上拉事件
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
   onReachBottom: function () {
-    //上拉  
+  
+  },
 
-    console.log("上拉");
-    var that = this;
-    //let _dhbottom = true;
-    //let _dhtop = true;
-  //  that.setData({
-  //    dhbottom: _dhbottom,
-     // dhtop: _dhtop,
-  //  })
-    console.log(that.data.dhbottom)
-    that.loadData(5);
-    wx.stopPullDownRefresh();
-
-  },
-  //导航初始化
-  dhInit: function () {
-    console.log("T10：导航初始化完毕！")
-  },
-  //导航事件
-  //导航事件
-  //导航事件
-  //导航事件
-  //导航事件
-  //首页-点击事件
-  dhtxt01Cllick: function (e) {
-    wx.redirectTo({
-      url: '../index/index'
-    })
-    console.log("F:商家首页")
-
-  },
-  //招聘群管-点击事件
-  dhtxt02Cllick: function () {
-    
-    wx.redirectTo({
-      url: '../zhaopin/zhaopin'
-    })
-    console.log("F:招聘群管")
-  },
-  //我的清单-点击事件
-  dhtxt03Cllick: function () {
-    
-    wx.redirectTo({
-      url: '../city/city'
-    })
-    console.log("F:全城活动")
-  },
-  //个人中心-点击事件
-  dhtxt04Cllick: function () {
-    
-    wx.redirectTo({
-      url: '../zhongxin/zhongxin'
-    })
-    console.log("F:个人中心")
-  },
-  //搜索页面-点击事件
-  ssClick:function(){
-    wx.navigateTo({
-      url: '../sousuo/sousuo'
-    })
-    console.log("F:搜索页面")
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
   }
 })
